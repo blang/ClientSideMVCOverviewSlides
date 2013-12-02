3bcce1239924a5a4d1c0dbd66d4cbbc91e07293d
Benedikt Lang <github@benediktlang.de>
2013-12-02 16:15:22 +0100

    feat(quiddles): better ngdirectives example

--- quiddles/ngdirectives.js
+++ quiddles/ngdirectives.js
@@ -1,12 +1,13 @@
 angular.module('proseminar', [])
     .controller('NgDirectivesCtrl', function($scope){
-        $scope.todos = [
+        $scope.todolist = [
             {text: 'Abspuelen', done: false},
             {text: 'Mails schreiben', done: false}, 
             {text: 'Mit Proseminar anfangen', done: false}
         ];
-        $scope.aufschieben = function(){
-            $scope.todos = [];
+        $scope.remaining = function(){
+            return $scope.todolist.filter(
+              function(item){return !item.done});
         };
         $scope.weghier = function(){
             $scope.successimg = 'img/nothingtodo.gif';
